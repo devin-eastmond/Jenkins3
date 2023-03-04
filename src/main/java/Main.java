df834acb123f4a63505a300c87374adf1173993f
Devin Eastmond <devinator9876@gmail.com>
2023-03-03 21:19:37 -0700

    bugfix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,40 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main (String[] args) {
+    while (true) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Enter command: ");
+
+      String input = scanner.nextLine();  // Read user input
+      if (Objects.equals(input, "quit")) {
+        break;
+      }
+      calculate(input.split(" "));
+    }
+  }
+
+  private static void calculate(String[] args) {
     Calculator calculator = new Calculator();
     switch (args[0]) {
       case "add":
-        calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
         break;
       case "subtract":
-        calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
         break;
       case "multiply":
-        calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
         break;
       case "divide":
-        calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        System.out.println(calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
         break;
       case "fibonacciNumberFinder":
-        calculator.fibonacciNumberFinder(Integer.parseInt(args[1]));
+        System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
         break;
       case "intToBinaryNumber":
-        calculator.intToBinaryNumber(Integer.parseInt(args[1]));
+        System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
         break;
     }
   }
